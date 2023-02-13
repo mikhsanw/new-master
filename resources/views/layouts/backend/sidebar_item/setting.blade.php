8a2035dbc87ad1f87dc5ba85c8301f886c8beb80
M. Ikhsan Wibowo <mikhsanw@riau.go.id>
2023-02-13 14:45:32 +0700

    first

---
+++ resources/views/layouts/backend/sidebar_item/setting.blade.php
@@ -0,0 +1,43 @@
+<!-- Control Sidebar -->
+<aside class="control-sidebar">
+	  
+      <div class="rpanel-title"><span class="pull-right btn btn-circle btn-danger"><i class="fa fa-close text-white" data-toggle="control-sidebar"></i></span> </div>  <!-- Create the tabs -->
+      <ul class="nav nav-tabs control-sidebar-tabs">
+        <!-- <li class="nav-item"><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-inbox"></i></a></li>
+        <li class="nav-item"><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-play"></i></a></li>
+      </ul> -->
+      <!-- Tab panes -->
+      <div class="tab-content">
+        <!-- Home tab content -->
+        <div class="tab-pane" id="control-sidebar-home-tab">
+            <div class="flexbox">
+              <a href="javascript:void(0)" class="text-grey">
+                  <i class="ti-more"></i>
+              </a>	
+              <p>Users</p>
+              <a href="javascript:void(0)" class="text-right text-grey"><i class="ti-plus"></i></a>
+            </div>
+            <div class="lookup lookup-sm lookup-right d-none d-lg-block">
+              <input type="text" name="s" placeholder="Search" class="w-p100">
+            </div>
+            </div>
+  
+        </div>
+        <!-- /.tab-pane -->
+        <!-- Settings tab content -->
+        <div class="tab-pane" id="control-sidebar-settings-tab">
+            <div class="flexbox">
+              <a href="javascript:void(0)" class="text-grey">
+                  <i class="ti-more"></i>
+              </a>	
+              <p>Todo List</p>
+              <a href="javascript:void(0)" class="text-right text-grey"><i class="ti-plus"></i></a>
+            </div>
+          <ul class="todo-list mt-20">
+              
+            </ul>
+        </div>
+        <!-- /.tab-pane -->
+      </div>
+    </aside>
+    <!-- /.control-sidebar -->
---
+++ routes/backend.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Backend Routes
+|--------------------------------------------------------------------------
+|
+|
+*/
+
+use Illuminate\Support\Facades\Route;
+
+Route::get(config('master.aplikasi.author').'/{folder}/{file}', 'jsController@backend');
+Route::get(config('master.aplikasi.author').'/{folder}/{id}/{file}', 'jsController@backendWithId');
+Route::get(config('master.aplikasi.author').'/{folder}/{link}/{kode}/{file}', 'jsController@backendWithKode');
+
+Route::get('/home', 'berandaController@index')->name('beranda.home');
+Route::group(['prefix' => config('master.url.admin')], function () {
+	// dashboard - beranda
+	// Route::get('/', 'berandaController@index')->name('beranda.index');
+
+	// Url Public
+    Route::group(['middleware' => ['throttle:5']], function () {
+		Route::post('lock-screen', 'userController@lockScreen');
+    });
+	//user ubah password
+	Route::get('user/ubahpassword/{id}', 'userController@ubahpassword')->name('user.ubahpassword');
+	Route::group(['middleware' => ['throttle:10']], function () {
+		Route::post('user/ubahpassword', 'userController@resetpassword')->name('user.store_ubahpassword');
+	});
+	Route::group(['middleware' => ['aksesmenu']], function () {
+        //user
+        Route::get('user/hapus/{id}', 'userController@hapus')->name('user.hapus');
+        Route::get('user/data', 'userController@data')->name('user.data');
+        Route::resource('user', 'userController');
+        //menu
+        Route::get('menu/hapus/{id}', 'menuController@hapus')->name('menu.hapus');
+        Route::get('menu/data', 'menuController@data')->name('menu.data');
+        Route::post('menu/susun-menu', 'menuController@susunMenu')->name('menu.susun-menu');
+        Route::get('menu/extract-menu', 'menuController@extract')->name('menu.extract-menu');
+        Route::resource('menu', 'menuController');
+        //aksesgrup
+        Route::get('aksesgrup/hapus/{id}', 'aksesgrupController@hapus')->name('aksesgrup.hapus');
+        Route::get('aksesgrup/data', 'aksesgrupController@data')->name('aksesgrup.data');
+        Route::get('aksesgrup/detail/data/{id}', 'aksesgrupController@data_detail')->name('aksesgrup.data_detail');
+        Route::resource('aksesgrup', 'aksesgrupController');
+        //aksesmenu
+        Route::get('aksesmenu/data/{id}', 'aksesmenuController@data')->name('aksesmenu.data');
+        Route::get('aksesmenu/create/{id}', 'aksesmenuController@create')->name('aksesmenu.create_id');
+        Route::resource('aksesmenu', 'aksesmenuController');
+        
+        // slider
+        Route::prefix('slider')->as('slider')->group(function () {
+            Route::get('/data', 'sliderController@data');
+            Route::get('/hapus/{id}', 'sliderController@hapus');
+        });
+        Route::resource('slider', 'sliderController');
+
+        // kontak
+        Route::prefix('kontak')->as('kontak')->group(function () {
+            Route::get('/data', 'kontakController@data');
+            Route::get('/hapus/{id}', 'kontakController@hapus');
+        });
+        Route::resource('kontak', 'kontakController');
+
+        // aplikasi
+        Route::prefix('aplikasi')->as('aplikasi')->group(function () {
+            Route::get('/data/{id?}', 'aplikasiController@data');
+            Route::get('/logo/{id}', 'aplikasiController@logo');
+            Route::post('/store_logo', 'aplikasiController@store_logo');
+            Route::get('/favicon/{id}', 'aplikasiController@favicon');
+            Route::post('/store_favicon', 'aplikasiController@store_favicon');
+        });
+        Route::resource('aplikasi', 'aplikasiController');   
+
+    });
+});
